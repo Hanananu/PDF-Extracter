5fe74619c57db434f1c7e0ff7c4d8dbd0b9a66ea
prabhath-kj <view.prabhath.kj@gmail.com>
2023-11-05 21:08:38 +0530

    Custom file filtering added for multer stroage

--- api/src/middlewares/uploadMiddleware.js
+++ api/src/middlewares/uploadMiddleware.js
@@ -1,22 +1,30 @@
-import multer from "multer"
-import { resolve } from 'path';
+import multer from "multer";
+import { resolve } from "path";
 import { generateUniqueFileName } from "../utils/fileUtils.js";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, resolve(__dirname, '../../../api','public', 'uploads'));
-    },
-    filename: function (req, file, cb) {
-      const destFileName = generateUniqueFileName("pdf");
-      cb(null, destFileName);
-    }
-  });
-  
-  const upload = multer({ storage: storage });
+  destination: function (req, file, cb) {
+    cb(null, resolve(__dirname, "../../../api", "public", "uploads"));
+  },
+  filename: function (req, file, cb) {
+    const destFileName = generateUniqueFileName("pdf");
+    cb(null, destFileName);
+  },
+});
 
-  export default upload
+const fileFilter = (req, file, cb) => {
+  let ext = path.extname(file.originalname);
+  if (ext !== ".pdf") {
+    return cb(new Error("File type not supported"), false);
+  }
+  cb(null, true);
+};
+
+const upload = multer({ storage: storage, fileFilter });
+
+export default upload;
