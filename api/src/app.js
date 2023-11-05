f09d44c5a61eb86a27e5548181907307d0ee282f
prabhath-kj <view.prabhath.kj@gmail.com>
2023-11-05 13:46:45 +0530

    Bug fixed /cors related

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import morgan from 'morgan';
-import helmet from 'helmet'; // Added Helmet for enhanced security headers
-import connectDB from './config/dbConfig.js';
-import * as dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js';
-import pdfRoutes from './routes/pdfRoutes.js';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet"; // Added Helmet for enhanced security headers
+import connectDB from "./config/dbConfig.js";
+import * as dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import pdfRoutes from "./routes/pdfRoutes.js";
 
 dotenv.config();
 
@@ -17,15 +17,18 @@ connectDB();
 
 // Middleware
 app.use(helmet()); // Use Helmet middleware for enhanced security headers
-app.use(cors({
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
-}));
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan('short'));
+app.use(morgan("short"));
 
 // Routes
-app.use('/api/user', userRoutes);
-app.use('/api/pdf', pdfRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/pdf", pdfRoutes);
 
 export default app;
