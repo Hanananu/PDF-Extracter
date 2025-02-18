33618a5153655f2a6fb248b36ee2058959dec2bd
hanan <hanananu899@gmail.com>
2025-02-18 18:20:30 +0530

    changed backend url

--- client/src/constants/constant.js
+++ client/src/constants/constant.js
@@ -1,2 +1,4 @@
 // export const BACKEND_URL="https://api-pdf-forge.onrender.com/api"
-export const BACKEND_URL="http://localhost:3001/api"
+// export const BACKEND_URL="http://localhost:3001/api"
+
+export const BACKEND_URL = "https://pdf-extracter-two.vercel.app"
